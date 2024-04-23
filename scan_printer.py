213584fb0fafaf9469e4d86352387e0e4c59a2e3
Jade233333 <123865201+Jade233333@users.noreply.github.com>
2024-04-23 14:06:14 +0700

    tqdm and output ips (#4)
    Co-authored-by: Kathy <yuxuan.zh.jade@gmail.com>

--- scan_printer.py
+++ scan_printer.py
@@ -1,6 +1,7 @@
 import requests
 import ipaddress
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 # Define the network address and subnet mask
 network_address = "172.19.4.0"
@@ -10,19 +11,28 @@ port = "631"
 # Create an IPv4 network object
 network = ipaddress.IPv4Network(f"{network_address}/{subnet_mask}")
 
+printer_ip = []
+
 # Function to check port status and write open IP addresses to a file
 def check_port(ip):
     url = f"http://{ip}:{port}"
     try:
         response = requests.get(url, timeout=3)
         if response.status_code == 200:
-            print(f"port {port} on {ip} is open")
-        else:
-            print(f"port {port} on {ip} is closed")
-    except requests.RequestException as e:
-        print(f"Failed to connect to {ip}:{port} : {e}")
+            return ip
+    except:
+        pass
+    return None
 
 # Use ThreadPoolExecutor to execute requests concurrently
 with ThreadPoolExecutor(max_workers=2048) as executor:
-    executor.map(check_port, network.hosts())
+    results = []
+    for result in tqdm(executor.map(check_port, network.hosts()), total=network.num_addresses):
+        results.append(result)
 
+# Filter out None values from results
+printer_ip = [ip for ip in results if ip]
+
+# Print ip one by one
+for ip in printer_ip:
+    print(ip)
