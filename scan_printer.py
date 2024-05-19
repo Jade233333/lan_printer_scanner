b6092bce58e87b1ee48934dcae14521642421ab0
Kathy <hatesjade@gmail.com>
2024-05-19 20:34:43 +0800

    format update

--- scan_printer.py
+++ scan_printer.py
@@ -8,14 +8,20 @@ import argparse
 # Define the network address and subnet mask
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="which port on IPs to scan", default="631")
-parser.add_argument("-i", "--ip", help="ip address to scan", default="192.168.31.0")
-parser.add_argument("-s", "--subnet", help="subnet mask to specify the IP range", default="255.255.255.0")
+parser.add_argument("-i", "--ip", help="ip address to scan", default="172.19.4.0")
+parser.add_argument(
+    "-s",
+    "--subnet",
+    help="subnet mask to specify the IP range",
+    default="255.255.252.0",
+)
 args = parser.parse_args()
 
 # Create an IPv4 network object
 network = ipaddress.IPv4Network(f"{args.ip}/{args.subnet}")
 
 printer_ip = []
+
 
 # Function to check port status and write open IP addresses to a file
 def check_port(ip):
@@ -24,14 +30,17 @@ def check_port(ip):
         response = requests.get(url, timeout=3)
         if response.status_code == 200:
             return ip
-    except:
+    except Exception:
         pass
     return None
+
 
 # Use ThreadPoolExecutor to execute requests concurrently
 with ThreadPoolExecutor(max_workers=2048) as executor:
     results = []
-    for result in tqdm(executor.map(check_port, network.hosts()), total=network.num_addresses):
+    for result in tqdm(
+        executor.map(check_port, network.hosts()), total=network.num_addresses
+    ):
         results.append(result)
 
 # Filter out None values from results
